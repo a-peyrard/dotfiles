858a9b36e1561fcbd0041c6feb93953335d1c561
Augustin Peyrard <augustin.peyrard@gmail.com>
2025-12-31 23:48:27 -0800

    vim: add autopair

---
+++ links/.config/nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,33 @@
+-- Auto-pairs for brackets, quotes, etc.
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = function()
+    local autopairs = require("nvim-autopairs")
+    
+    autopairs.setup({
+      check_ts = true, -- Use treesitter to check for pair
+      ts_config = {
+        lua = { "string" }, -- Don't add pairs in lua string treesitter nodes
+        javascript = { "template_string" },
+        java = false, -- Don't check treesitter on java
+      },
+      disable_filetype = { "TelescopePrompt", "vim" },
+      fast_wrap = {
+        map = "<M-e>", -- Alt+e to fast wrap
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = [=[[%'%"%>%]%)%}%,]]=],
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        highlight = "Search",
+        highlight_grey = "Comment",
+      },
+    })
+
+    -- Integration with nvim-cmp (auto-completion)
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    local cmp = require("cmp")
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end,
+}
