9cbc10519ba7e60f9b25f13426f690210c70ba1f
Augustin Peyrard <augustin.peyrard@gmail.com>
2026-01-18 20:49:02 -0800

    nvim: add jetbrain theme
    not sure I will really use it tho

--- links/.config/nvim/lua/plugins/colorscheme.lua
+++ links/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,30 @@
 -- Colorscheme configuration
 return {
-  "folke/tokyonight.nvim",
-  lazy = false, -- Load immediately during startup
-  priority = 1000, -- Load before other plugins
-  config = function()
-    require("tokyonight").setup({
-      style = "storm", -- darker blue variant
-      on_colors = function(colors)
-        colors.border = colors.orange -- orange borders like server config
-      end,
-    })
-    vim.cmd([[colorscheme tokyonight-storm]])
-  end,
+  -- Primary theme (loaded by default)
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "storm",
+        on_colors = function(colors)
+          colors.border = colors.orange
+        end,
+      })
+      vim.cmd([[colorscheme tokyonight-storm]])
+    end,
+  },
+
+  -- JetBrains-inspired theme (available via <leader>ft)
+  {
+    "nickkadutskyi/jb.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      snacks = {
+        explorer = { enabled = true },
+      },
+    },
+  },
 }
