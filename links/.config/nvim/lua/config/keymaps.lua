8c5a520ceb52a21e87c9840074f0cac61761b66c
Augustin Peyrard <augustin.peyrard@gmail.com>
2026-01-22 14:58:21 -0800

    nvim: adds <leader>/ for noh

--- links/.config/nvim/lua/config/keymaps.lua
+++ links/.config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,6 @@
 
 -- Exit insert mode with jk
 vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode' })
+
+-- Clear search highlighting
+vim.keymap.set('n', '<leader>/', '<cmd>nohlsearch<cr>', { desc = 'Clear search highlight' })
